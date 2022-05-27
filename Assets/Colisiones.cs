51f5adc1bb3bc5b83f2ee71ce566ce733ad64433
Jeremias_Abulafia <jeremiasabulafia@gmail.com>
2022-05-27 10:07:47 -0300

    Initial commit

---
+++ Assets/Colisiones.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+    }
+    void OnCollisionExit()
+    {
+        Debug.Log("Dejo de hacer contacto");
+    }
+    void OnCollisionStay()
+    {
+        Debug.Log("Esta en Contacto");
+    }
+}
---
+++ Assets/ColisionesPlayer1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionesPlayer1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        /*if (col.gameObject.name == "DeathWall")
+        {
+            Destroy(gameObject);
+        }*/
+        if (col.gameObject.name == "DeathWall")
+        {
+            transform.position = new Vector3(-3.35f,0.5f,-3.61f);
+            transform.eulerAngles = new Vector3(0,90,0);
+        }
+        if (col.gameObject.name == "DeathPlane")
+        {
+            transform.position = new Vector3(-3.35f,0.5f,-3.61f);
+            transform.eulerAngles = new Vector3(0,90,0);
+        }
+    }
+}
