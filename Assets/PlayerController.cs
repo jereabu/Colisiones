59445293f69f79dfb9430811885d5724c2770ee1
Jeremias_Abulafia <jeremiasabulafia@gmail.com>
2022-05-27 10:29:10 -0300

    Improved controller accesibility

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,19 +15,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
         transform.Translate(0,0,movementSpeed);
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
         transform.Translate(0,0,-movementSpeed);
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
         transform.Rotate(0,rotationSpeed,0);
         }
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
         transform.Rotate(0,-rotationSpeed,0);
         }
